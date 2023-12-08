4eb56e009cae81be1455252d4fb0bd4db7f0f0c8
Pluto Rotegott <rotegott@dogma-project.org>
2023-12-08 13:19:09 +0200

    key generation function with constants

--- src/controllers/keys.ts
+++ src/controllers/keys.ts
@@ -3,6 +3,7 @@ import { State, Keys } from "@dogma-project/core-meta";
 import ClientError from "../helpers/clientError";
 import { CLIENT_STATUSES, ERRORS } from "../constants";
 import { C_Event, C_System, C_Keys } from "@dogma-project/constants-meta";
+
 export async function createKey(
   req: Request,
   res: Response,
@@ -11,48 +12,44 @@ export async function createKey(
   const { length, name, type } = req.body;
   // add validation
   const prefix = State.stateManager.state[C_Event.Type.prefix] || "default";
+  const keyType = type as C_Keys.Type;
+  const keyLength = length as 1024 | 2048 | 4096;
+  const keyName = name as string;
+  let keyEvent: C_Event.Type;
   try {
-    if (type === "master") {
-      const keyState = State.stateManager.state[C_Event.Type.masterKey];
-      if (!keyState || keyState > C_System.States.empty) {
+    switch (keyType) {
+      case C_Keys.Type.masterKey:
+        keyEvent = C_Event.Type.masterKey;
+        State.storage.user.name = keyName;
+        break;
+      case C_Keys.Type.nodeKey:
+        keyEvent = C_Event.Type.nodeKey;
+        State.storage.node.name = keyName;
+        break;
+      default:
         return next(
           new ClientError({
             status: CLIENT_STATUSES.BAD_REQUEST,
-            code: ERRORS.KEY_IS_NOT_EMPTY,
-            payload: {
-              state: keyState,
-            },
+            code: ERRORS.UNKNOWN_KEY_TYPE,
+            payload: { type },
           })
         );
-      }
-      State.storage.user.name = name;
-      await Keys.createKeyPair(C_Keys.Type.masterKey, prefix, length);
-      State.stateManager.emit(C_Event.Type.masterKey, C_System.States.ready);
-    } else if (type === "node") {
-      const keyState = State.stateManager.state[C_Event.Type.nodeKey];
-      if (!keyState || keyState > C_System.States.empty) {
-        return next(
-          new ClientError({
-            status: CLIENT_STATUSES.BAD_REQUEST,
-            code: ERRORS.KEY_IS_NOT_EMPTY,
-            payload: {
-              state: keyState,
-            },
-          })
-        );
-      }
-      State.storage.node.name = name;
-      await Keys.createKeyPair(C_Keys.Type.nodeKey, prefix, length);
-      State.stateManager.emit(C_Event.Type.nodeKey, C_System.States.ready);
-    } else {
+    }
+    const keyState = State.stateManager.state[keyEvent];
+    if (!keyState || keyState > C_System.States.empty) {
       return next(
         new ClientError({
           status: CLIENT_STATUSES.BAD_REQUEST,
-          code: ERRORS.UNKNOWN_KEY_TYPE,
-          payload: { type },
+          code: ERRORS.KEY_IS_NOT_EMPTY,
+          payload: {
+            state: keyState,
+          },
         })
       );
     }
+
+    await Keys.createKeyPair(keyType, prefix, keyLength);
+    State.stateManager.emit(keyEvent, C_System.States.ready);
     return res.json({
       result: true,
     });
